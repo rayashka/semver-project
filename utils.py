1aa8d8e34f8cb6e7b86ca22b8e69bf25936c00ce
Rayana Malsagova <raina20070707@gmail.co>
2025-10-23 20:56:18 +0300

    feat: add user greeting and version info
    - Add greet_user function for personalized greetings
    - Add get_version function
    - Update main app to show version and demo new feature
    
    This is a MINOR version update (v1.1.0) because:
    - Added new functionality (greet_user)
    - Backwards compatible with v1.0.0
    - No breaking changes

--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@
 
 def get_greeting():
     return "Hello World!"
+
+def get_version():
+    return "1.1.0"
+
+def greet_user(name):
+    """Новая функция - приветствие по имени"""
+    return f"Hello, {name}!"
